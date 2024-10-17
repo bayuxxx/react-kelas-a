7ce13b81e2ecf4c4b55ea954cb3a3004cb005c8f
I GEDE BAYU BALAWA <130529886+bayuxxx@users.noreply.github.com>
2024-10-17 09:24:29 +0800

    halo

--- src/components/sortinglist.jsx
+++ src/components/sortinglist.jsx
@@ -2,10 +2,10 @@ export default function ListSortingq({ nama, logo, harga }) {
   return (
     <>
       {harga > 10000 ? (
-        <li>
+        <>
           {" "}
           {nama} {harga} {logo}
-        </li>
+        </>
       ) : (
         ""
       )}
